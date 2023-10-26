6636df3c9625e3ba07353a3efb21659d3c90a65b
alfian-f <alfian.0615@gmail.com>
2023-10-26 22:28:08 +0700

    added a way to edit books through /admin, created new userItem model

--- main/models.py
+++ main/models.py
@@ -9,3 +9,13 @@ class Book(models.Model):
     image_s = models.URLField(null=True, blank=True)
     image_m = models.URLField(null=True, blank=True)
     image_l = models.URLField(null=True, blank=True)
+
+class userItem(models.Model):
+    isbn = models.TextField(null=True, blank=True)
+    title = models.TextField(null=True, blank=True)
+    author = models.TextField(null=True, blank=True)
+    year = models.IntegerField(null=True, blank=True)
+    publisher = models.TextField(null=True, blank=True)
+    image_s = models.URLField(null=True, blank=True)
+    image_m = models.URLField(null=True, blank=True)
+    image_l = models.URLField(null=True, blank=True)
