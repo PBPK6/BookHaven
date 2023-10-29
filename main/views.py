9602701bfeb74217fa55e708aad0c20ed9cabcd7
alfian-f <alfian.0615@gmail.com>
2023-10-29 20:08:48 +0700

    added favicon and title

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponseRedirect, JsonResponse
+from django.http import HttpResponseRedirect, JsonResponse, HttpResponseNotFound
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
