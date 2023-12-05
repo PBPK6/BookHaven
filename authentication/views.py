35e7f9be9e0fec5303260b7657e13d675d7f3c0a
goosetea04 <gusti.fatu@gmail.com>
2023-12-05 20:27:54 +0700

    completed logout and slight problem with register

--- authentication/views.py
+++ authentication/views.py
@@ -4,6 +4,10 @@ from django.contrib.auth import authenticate, login as auth_login
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import logout as auth_logout
+import json
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.views.decorators.http import require_POST
 
 @csrf_exempt
 def login(request):
@@ -48,3 +52,23 @@ def logout(request):
         "status": False,
         "message": "Logout failed."
         }, status=401)
+
+@require_POST   
+@csrf_exempt
+def register(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        form = UserCreationForm({"username": data['username'], "password1": data['password1'], "password2": data['password2']})
+        if form.is_valid():
+            form.save()
+            return JsonResponse({
+                "status": True,
+                "message": "User successfully registered!"
+            }, status=200)
+        else:
+            test = messages.get_messages(request)
+
+            return JsonResponse({
+                "status": False,
+                "message": "Register failed!",
+            }, status=401)
